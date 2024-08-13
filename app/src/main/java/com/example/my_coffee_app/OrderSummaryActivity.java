a070cfba7f692bca9ce5c11f0cf3073c8202e35a
mengkim168 <mengkimtang@gmail.com>
2024-08-14 02:33:11 +0700

    Update UI

--- app/src/main/java/com/example/my_coffee_app/OrderSummaryActivity.java
+++ app/src/main/java/com/example/my_coffee_app/OrderSummaryActivity.java
@@ -1,10 +1,14 @@
 package com.example.my_coffee_app;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,12 +17,15 @@ import java.util.ArrayList;
 public class OrderSummaryActivity extends AppCompatActivity {
 
     private int totalAmount = 0;
+    private Button btnBack,btnCompleteOrder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_summary);
 
+        btnBack = (Button) findViewById(R.id.btnBack);
+        btnCompleteOrder =(Button) findViewById(R.id.btnCompleteOrder);
         LinearLayout orderSummaryListLayout = findViewById(R.id.orderSummaryListLayout);
         TextView totalOrderAmountText = findViewById(R.id.totalOrderAmountText);
 
@@ -42,5 +49,28 @@ public class OrderSummaryActivity extends AppCompatActivity {
         }
 
         totalOrderAmountText.setText("Total: $" + totalAmount);
+
+        btnBack.setOnClickListener(v -> finish());
+        btnCompleteOrder.setOnClickListener(v -> {
+            showOrderCompleteToast();
+            resetOrderList(orderList);
+            Intent intent = new Intent(OrderSummaryActivity.this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // Clear the activity stack
+            startActivity(intent);
+            finish();
+        });
+
     }
+
+    private void showOrderCompleteToast() {
+        Toast.makeText(this, "Order Completed. Total Amount: $" + totalAmount, Toast.LENGTH_LONG).show();
+    }
+
+    private void resetOrderList(ArrayList<Coffee> orderList) {
+        for (Coffee coffee : orderList) {
+            coffee.setQuantity(0);
+        }
+    }
+
+
 }
